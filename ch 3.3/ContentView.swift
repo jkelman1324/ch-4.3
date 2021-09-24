6894d7de722ae6286f0fb873658773efd511720c
jkelman1324 <51901448+jkelman1324@users.noreply.github.com>
2021-09-24 12:50:49 -0700

    Initial Commit

---
+++ ch 3.3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ch 3.3
+//
+//  Created by Jake Kelman on 9/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ch 3.3/ch_3_3App.swift
@@ -0,0 +1,17 @@
+//
+//  ch_3_3App.swift
+//  ch 3.3
+//
+//  Created by Jake Kelman on 9/24/21.
+//
+
+import SwiftUI
+
+@main
+struct ch_3_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
