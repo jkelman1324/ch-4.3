e3d69be7970107e902b66661f29eae0d4d32c3c6
jkelman1324 <51901448+jkelman1324@users.noreply.github.com>
2021-09-24 13:15:08 -0700

    challenge 4.3

--- ch 3.3/ContentView.swift
+++ ch 3.3/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tabtag:Int = 1
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView (selection: $tabtag) {
+            Text("This tab's tag is " + String(tabtag))
+            .tabItem {
+                Image(systemName: "tortoise")
+                Text("Tab 1")
+            }.tag(1)
+           
+            Button(action: {
+                tabtag = 3
+            }, label: {
+                Text("Take me to tab 3")
+            })
+            .tabItem {
+                Image(systemName: "arrow.right")
+                Text("Tab 2")
+            }.tag(2)
+             
+            List(0..<100, id: \.self) { num in
+                Text("This is Tab 3!")
+            }.tabItem {
+                VStack {
+                    Image(systemName: "hands.clap")
+                    Text("Tab 3")
+                }
+            }.tag(3)
+        }
     }
 }
 
